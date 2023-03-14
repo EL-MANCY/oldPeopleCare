85c28a01a225d6d10f002f833011cf1c8aa2c466
EL-MANCY <abdelrahmanelmancy@gmail.com>
2023-03-14 14:39:26 +0200

    latest v

---
+++ app/src/main/java/com/example/oldpeoplecareapp/ui/CaregiverPath/AllPatients/AllPatientsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.oldpeoplecareapp.ui.CaregiverPath.AllPatients
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.oldpeoplecareapp.R
+
+class AllPatientsFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_all_patients, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/oldpeoplecareapp/ui/CaregiverPath/CaregiverNotifications/CaregiverNotificationsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.oldpeoplecareapp.ui.CaregiverPath.CaregiverNotifications
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.oldpeoplecareapp.R
+
+class CaregiverNotificationsFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_caregiver_notifications, container, false)
+    }
+
+
+}
