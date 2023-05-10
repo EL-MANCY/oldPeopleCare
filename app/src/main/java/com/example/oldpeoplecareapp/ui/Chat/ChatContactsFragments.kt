5cad82b804ef4f5c005f9c596862a5c8053978e9
EL-MANCY <abdelrahmanelmancy@gmail.com>
2023-05-10 23:30:36 +0300

    Chat Design

---
+++ app/src/main/java/com/example/oldpeoplecareapp/ui/Chat/ChatContactsFragments.kt
@@ -0,0 +1,18 @@
+package com.example.oldpeoplecareapp.ui.Chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.oldpeoplecareapp.R
+
+
+class ChatContactsFragments : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_chat_contacts_fragments, container, false)
+    }
+
+}
